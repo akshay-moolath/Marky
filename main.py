6fae24a840ae4920f2f5ca8c3193ddc6cbd1f268
akshay-moolath <akmoolath@gmail.com>
2025-12-01 23:46:11 +0530

    added downloading buttong to download text extracted from markdown using beautifulsoup.STABLE

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from bs4 import BeautifulSoup
+import markdown
+import bleach
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+ALLOWED_TAGS = list(bleach.sanitizer.ALLOWED_TAGS) + [
+    "p", "pre", "code", "hr", "br", "h1", "h2", "h3", "h4", "h5", "h6",
+    "table", "thead", "tbody", "tr", "th", "td", "ul", "ol", "li"
+]
+ALLOWED_ATTRIBUTES = {
+    **bleach.sanitizer.ALLOWED_ATTRIBUTES,
+    "a": ["href", "title", "rel", "target"],
+    "img": ["src", "alt", "title"],
+    "th": ["colspan", "rowspan"],
+    "td": ["colspan", "rowspan"],
+}
+
+
+def markdown_to_safe_html(md_text: str) -> str:
+    raw_html = markdown.markdown(
+    md_text,
+    extensions=["fenced_code", "tables", "toc"]
+    )
+    clean = bleach.clean(
+        raw_html,
+        tags=ALLOWED_TAGS,
+        attributes=ALLOWED_ATTRIBUTES,
+        strip=True)
+    clean = bleach.linkify(clean)
+    clean = clean.replace("<a ", '<a target="_blank" rel="noopener noreferrer" ')
+
+    return clean
+
+def extract_text_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+    text = soup.get_text(separator="\n")
+    return text.strip()
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("preview.html", {"request": request, "html": ""})
+
+@app.post("/render", response_class=HTMLResponse)
+async def render(request: Request, md: str = Form(...)):
+    html= markdown_to_safe_html(md)
+    plain = extract_text_from_html(html)
+    return templates.TemplateResponse("preview.html", {"request": request, "html": html, "md": md})
+
+
+
+
